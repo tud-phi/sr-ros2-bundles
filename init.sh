d5b6986a7459da706c6bfe71b17eca01e4336e27
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-10-30 11:38:22 +0100

    Start working on `sr-ros2-core-ubuntu-with-audio`

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 # allow access to X server
 xhost +local:all
 
+# create Pulseaudio socket
+pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
+
 # set USB latency to 1ms for fast Dynamixel communication
 echo 1 | sudo tee /sys/bus/usb-serial/devices/ttyUSB0/latency_timer
