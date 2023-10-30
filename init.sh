b1adfb95733ca45b21afe0a06dfd069d3741a4fd
Maximilian Stolzle <maximilian@stoelzle.ch>
2023-10-30 14:47:18 +0100

    Add more audio config for pulseaudio

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
+# set environment variable with current user and group id
+export CURRENT_UID=$(id -u):$(id -g)
+
 # allow access to X server
 xhost +local:all
 
 # create Pulseaudio socket
 pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
+# copy the config file to /tmp/pulseaudio_clientclient.conf
+cp ./utils/pulseaudio/client.conf /tmp/pulseaudio_client.conf
 
 # set USB latency to 1ms for fast Dynamixel communication
 echo 1 | sudo tee /sys/bus/usb-serial/devices/ttyUSB0/latency_timer
